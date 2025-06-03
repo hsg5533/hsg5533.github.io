3bb65e3d65de6d49f25033f80525ece21bbd8881
hsg5533 <hsg5533@naver.com>
2025-06-03 12:14:56 +0900

    edit: mobile design

--- src/components/Sec4.tsx
+++ src/components/Sec4.tsx
@@ -12,19 +12,19 @@ export default function Sec4() {
       <div className="sec4-box">
         <div className="contact-item">
           <a href="https://open.kakao.com/me/hsg5533">
-            <img src={kakao} alt="" />
+            <img src={kakao} alt="kakao" />
             <div className="contact-item-desc">KAKAO</div>
           </a>
         </div>
         <div className="contact-item">
           <a href="mailto:hsg55334@naver.com">
-            <img src={email} alt="" />
+            <img src={email} alt="email" />
             <div className="contact-item-desc">EMAIL</div>
           </a>
         </div>
         <div className="contact-item">
           <a href="https://github.com/hsg5533">
-            <img src={github} alt="" />
+            <img src={github} alt="guthub" />
             <div className="contact-item-desc">GITHUB PROFILE</div>
           </a>
         </div>
